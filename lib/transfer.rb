d1a5294593f3d4a1662e2dea043bc31dde042f85
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 20:21:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,7 +5,7 @@ class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
   
-  def initialize(receiver, sender, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
