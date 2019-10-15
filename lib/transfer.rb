7bc60b1ecd3d8fefa08911010bef6e3855787470
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 19:34:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  # your code here
+  
+  def initialize(giver_name, receiver_name, amount)
+    @giver_name = giver_name
+    @receiver_name = receiver_name
+    @amount = amount
+  end
+  
 end
